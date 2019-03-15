fae1fc5bd4ae713aa8fc7de0836669613e937b95
Ilya Filippov <89099109104@mail.ru>
2019-03-15 19:15:12 +0300

    Add IDs to messages

--- gomoku-server/index.js
+++ gomoku-server/index.js
@@ -1,6 +1,7 @@
 const cors = require('cors')
 const express = require('express')
 const socketIo = require('socket.io')
+const uuidv4 = require('uuid/v4')
 
 const app = express()
 const PORT = 4000
@@ -11,13 +12,16 @@ const server = app.listen(PORT, () => {
   console.info(`The server is listening on ${PORT}.`)
 })
 
-io = socketIo(server)
+const io = socketIo(server)
 io.on('connection', socket => {
   console.info('User connected.')
-  socket.emit('server.message', 'Welcome to Gomoku game chat.')
+  socket.emit('server.message', { id: uuidv4(), messageText: 'Welcome to Gomoku game chat.' })
 
-  socket.on('client.message', message => {
-    io.emit('server.message', message)
+  socket.on('client.message', messageText => {
+    io.emit('server.message', {
+      id: uuidv4(),
+      messageText
+    })
   })
 
   socket.on('disconnect', () => {
