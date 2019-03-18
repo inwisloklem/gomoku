82d1964719e3220bea06e75064f35352822ebcf6
Ilya Filippov <89099109104@mail.ru>
2019-03-18 16:51:14 +0300

    Move socket init to App

--- gomoku-client/src/components/App.jsx
+++ gomoku-client/src/components/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
+import StoreContext, { initialState, reducer } from './StoreContext'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Lobby from './Lobby'
 import Login from './Login'
+import socketIoClient from 'socket.io-client'
+import styles from './App.module.sass'
+
+const ENDPOINT = '//localhost:4000'
+let socket
 
 function App () {
-  return (
-    <Router>
-      <Route exact path='/' component={Login} />
-      <Route path='/lobby' component={Lobby} />
-    </Router>
+  const [isConnected, setIsConnected] = useState(false)
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    socket = socketIoClient(ENDPOINT)
+    socket.on('connect', () => {
+      setIsConnected(true)
+    })
+  }, [])
+
+  return isConnected ? (
+    <StoreContext.Provider value={{ socket, state, dispatch }}>
+      <Router>
+        <Route exact path='/' component={Login} />
+        <Route path='/lobby' component={Lobby} />
+      </Router>
+    </StoreContext.Provider>
+  ) : (
+    <main className={styles.app}>
+      <h1 className={styles.title}>Connecting to socket...</h1>
+    </main>
   )
 }
 
