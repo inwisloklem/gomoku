c9745287059894e3fe772a34e1cbd701f7e54a17
inwisloklem <89099109104@mail.ru>
2019-04-13 17:03:46 +0300

    Make Game component

---
+++ gomoku-client/src/components/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Game.module.sass'
+
+function Game () {
+  return (
+    <main className={styles.game}>
+      <h1 className={styles.title}>Game component</h1>
+    </main>
+  )
+}
+
+export default Game
