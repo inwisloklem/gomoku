07015bd8f16f19233149a283ac8fd59d9f65930c
inwisloklem <89099109104@mail.ru>
2019-04-22 23:00:54 +0300

    Draw field

--- gomoku-client/src/components/Game.jsx
+++ gomoku-client/src/components/Game.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Game.module.sass'
 
+const FIELD_WIDTH = 20
+const FIELD_HEIGHT = 20
+
+const makeInitFieldState = (width, height) => new Array(height).fill(new Array(width).fill(null))
+
 function Game () {
+  const [field] = useState(makeInitFieldState(FIELD_WIDTH, FIELD_HEIGHT))
+
   return (
     <main className={styles.game}>
-      <h1 className={styles.title}>Game component</h1>
+      <div className={styles.field}>
+        {field.map(row => (
+          <div className={styles.row}>
+            {row.map(value => (
+              <button className={styles.button} type='button'>
+                {value}
+              </button>
+            ))}
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
