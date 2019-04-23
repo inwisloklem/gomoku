8e5a70040e0016c92189535f04843c80599e0eb6
inwisloklem <89099109104@mail.ru>
2019-04-24 00:01:30 +0300

    Add some game logic

--- gomoku-client/src/components/Game.jsx
+++ gomoku-client/src/components/Game.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import clone from '../clone'
 import styles from './Game.module.sass'
 
 const FIELD_WIDTH = 20
@@ -7,15 +8,29 @@ const FIELD_HEIGHT = 20
 const makeInitFieldState = (width, height) => new Array(height).fill(new Array(width).fill(null))
 
 function Game () {
-  const [field] = useState(makeInitFieldState(FIELD_WIDTH, FIELD_HEIGHT))
+  const [field, setField] = useState(makeInitFieldState(FIELD_WIDTH, FIELD_HEIGHT))
+
+  const handleClick = e => {
+    const [x, y] = e.target.dataset.square.match(/\d+/g)
+    const updatedField = clone(field)
+    updatedField[x][y] = 'X'
+    setField(updatedField)
+  }
 
   return (
     <main className={styles.game}>
+      <h1 className={styles.title}>Game</h1>
       <div className={styles.field}>
         {field.map((row, x) => (
           <div className={styles.row} key={x}>
             {row.map((value, y) => (
-              <button className={styles.button} key={y} type='button'>
+              <button
+                className={styles.button}
+                data-square={`[${x}, ${y}]`}
+                key={y}
+                onClick={handleClick}
+                type='button'
+              >
                 {value}
               </button>
             ))}
